c58dd27ca97dfbeb0c4d67fd59600e3c73fec8d1
Dan Nguyen <dansonguyen@gmail.com>
2020-11-10 20:59:19 -0600

    blacked scrape.py

--- scrape/scraper.py
+++ scrape/scraper.py
@@ -16,47 +16,61 @@ from pathlib import Path
 from sys import stderr, stdout
 from typing import Dict as DictType, List as ListType
 
-SRC_PATH = Path('docs/index.html')
+SRC_PATH = Path("docs/index.html")
 
-OUT_HEADER = ('agency', 'last_name',  'first_name', 'middle_name', 'recent_employment', 'funding_source', 'is_team_lead', 'full_name', 'row_index')
-TEAM_LEAD_TXT = ', Team Lead' # this is in the name of the first person in each row
+OUT_HEADER = (
+    "agency",
+    "last_name",
+    "first_name",
+    "middle_name",
+    "recent_employment",
+    "funding_source",
+    "is_team_lead",
+    "full_name",
+    "row_index",
+)
+TEAM_LEAD_TXT = ", Team Lead"  # this is in the name of the first person in each row
 
-def parse_page(html:str) -> ListType[DictType]:
+
+def parse_page(html: str) -> ListType[DictType]:
     data = []
 
     doc = hparse(html)
-    for hed in doc.cssselect('h2'):
+    for hed in doc.cssselect("h2"):
         agency = hed.text
-        for i, row in enumerate(hed.getnext().cssselect('tbody tr'), 1):
-            cells = row.cssselect('td')
-            if (i == 1 and TEAM_LEAD_TXT not in cells[0].text) or (i != 1 and ',' in cells[0].text):
-                # just making sure "Team Lead" appears only in the first row and nowhere else
-                raise ValueError('what the f')
+        for i, row in enumerate(hed.getnext().cssselect("tbody tr"), 1):
+            cells = row.cssselect("td")
+            # just making sure "Team Lead" appears only in the first row and nowhere else
+            # if (i == 1 and TEAM_LEAD_TXT not in cells[0].text) or (
+            #     i != 1 and "," in cells[0].text
+            # ):
+            #     raise ValueError("what the f")
 
             d = {
-                    'agency': agency,
-                    'full_name': cells[0].text.replace(TEAM_LEAD_TXT, ''),
-                    'recent_employment': cells[1].text,
-                    'funding_source': cells[2].text,
-                    'is_team_lead': True if i == 1 else False,
-                    'row_index': i,
-                }
+                "agency": agency,
+                "full_name": cells[0].text.replace(TEAM_LEAD_TXT, ""),
+                "recent_employment": cells[1].text,
+                "funding_source": cells[2].text,
+                "is_team_lead": True if i == 1 else False,
+                "row_index": i,
+            }
 
             # sloppy but whatever
-            d['first_name'], d['middle_name'], *lname = d['full_name'].split(' ', 2)
-            d['last_name'] = lname[0] if lname else d.pop('middle_name')
+            d["first_name"], d["middle_name"], *lname = d["full_name"].split(" ", 2)
+            d["last_name"] = lname[0] if lname else d.pop("middle_name")
             data.append(d)
 
     return data
 
+
 def main():
     data = parse_page(SRC_PATH.read_text())
-    stderr.write('Parsed %s data rows from: %s\n' % (len(data), SRC_PATH))
+    stderr.write("Parsed %s data rows from: %s\n" % (len(data), SRC_PATH))
 
     outs = csv.DictWriter(stdout, fieldnames=OUT_HEADER)
     outs.writeheader()
     outs.writerows(data)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
